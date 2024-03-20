16676d4d3aa2e894932aeca3cfa8339001802e6f
Hersh Nevgi <h.nevgi@gmail.com>
2024-03-20 23:38:20 +1100

    build voronoi

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,31 @@ fn main() {
 }
 
 struct Model {
-    sites: Vec<voronoice::Point>,
+    sites: Vec<Point>,
+    voronoi: Voronoi,
 }
 
 fn model(app: &App) -> Model {
+    let winrect = app.window_rect();
+
     let site_count = 50;
-    let sites = (0..site_count)
-        .map(|_| voronoice::Point {
-            x: random_range(
-                app.window_rect().left().into(),
-                app.window_rect().right().into(),
-            ),
-            y: random_range(
-                app.window_rect().left().into(),
-                app.window_rect().right().into(),
-            ),
+    let sites: Vec<Point> = (0..site_count)
+        .map(|_| Point {
+            x: random_range(winrect.left().into(), winrect.right().into()),
+            y: random_range(winrect.left().into(), winrect.right().into()),
         })
         .collect();
-    Model { sites }
+
+    let voronoi = VoronoiBuilder::default()
+        .set_sites(sites.clone())
+        .set_bounding_box(BoundingBox::new_centered(
+            winrect.w().into(),
+            winrect.h().into(),
+        ))
+        .build()
+        .expect("Provided sites don't generate a valid voronoi graph");
+
+    Model { sites, voronoi }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
@@ -31,11 +38,14 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {}
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
+    // draw points
     model.sites.iter().for_each(|site| {
         draw.ellipse()
             .x_y(site.x as f32, site.y as f32)
             .radius(1.0)
             .color(WHITE);
     });
+    // draw cell bounds
+
     draw.to_frame(app, &frame).unwrap();
 }
