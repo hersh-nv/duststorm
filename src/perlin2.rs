ed7080b60ef7649e7088ee437019554a1bb29ce0
hersh-nv <h.nevgi@gmail.com>
2024-04-24 14:13:17 +1000

    add ttl to agents

--- src/perlin2.rs
+++ src/perlin2.rs
@@ -4,6 +4,7 @@ use nannou::noise::{NoiseFn, Perlin, Seedable};
 use nannou::prelude::*;
 
 pub mod pos;
+use nannou::state::Time;
 use pos::Pos;
 
 fn main() {
@@ -13,24 +14,43 @@ fn main() {
 #[derive(Clone)]
 struct Agent {
     pos: Pos,       // (x,y) position
-    step_size: f32, // ??
+    step_size: f32, // in pixels
+    ttl: f32,       // how many seconds to survive before regenerating in random location
 }
 
 impl Agent {
-    pub fn update(&mut self, noise: Perlin, noise_scale: f64, target: Pos) {
-        // take a fixed step in the noise direction
-        let angle = noise.get([
-            self.pos.x as f64 / noise_scale,
-            self.pos.y as f64 / noise_scale,
-        ]) as f32;
-        let angle = angle * 2.0 * PI;
-        self.pos.x += angle.cos() * self.step_size;
-        self.pos.y += angle.sin() * self.step_size;
-        // then take a proportional step in the target direction
-        let dxy = target - self.pos;
-        let dxy = dxy * 0.002; // acceleration factor, tweak for best results
-        self.pos.x += dxy.x;
-        self.pos.y += dxy.y;
+    pub fn new(win: Rect) -> Agent {
+        Agent {
+            pos: Pos::new(
+                random_range(win.left(), win.right()),
+                random_range(win.bottom(), win.top()),
+            ),
+            step_size: 1f32,
+            ttl: random_range(2.0, 10.0),
+        }
+    }
+
+    pub fn update(&mut self, noise: Perlin, noise_scale: f64, target: Pos, win: Rect, time: Time) {
+        if self.ttl < 0.0 {
+            *self = Agent::new(win);
+        } else {
+            // take a fixed step in the noise direction
+            let angle = noise.get([
+                self.pos.x as f64 / noise_scale,
+                self.pos.y as f64 / noise_scale,
+                time.since_start.as_secs_f64() / 40.0,
+            ]) as f32;
+            let angle = angle * 2.0 * PI;
+            self.pos.x += angle.cos() * self.step_size;
+            self.pos.y += angle.sin() * self.step_size;
+            // then take a proportional step in the target direction
+            let dxy = target - self.pos;
+            let dxy = dxy * 0.002; // acceleration factor, tweak for best results
+            self.pos.x += dxy.x;
+            self.pos.y += dxy.y;
+            // update ttl
+            self.ttl -= time.since_prev_update.as_secs_f32();
+        }
     }
 }
 
@@ -44,19 +64,11 @@ struct Model {
 
 impl Model {
     pub fn new(win: Rect) -> Self {
-        let agent_count = 400;
-        let noise_scale = 200.0;
+        let agent_count = 800;
+        let noise_scale = 800.0;
         let noise_seed = random::<u32>();
         let perlin = Perlin::new().set_seed(noise_seed);
-        let agents = (0..agent_count)
-            .map(|_| Agent {
-                pos: Pos::new(
-                    random_range(win.left(), win.right()),
-                    random_range(win.bottom(), win.top()),
-                ),
-                step_size: 1f32,
-            })
-            .collect();
+        let agents = (0..agent_count).map(|_| Agent::new(win)).collect();
 
         Model {
             perlin,
@@ -69,16 +81,7 @@ impl Model {
 
     pub fn reset_agents(&mut self) {
         self.agents = (0..self.agents.len())
-            .map(|_| {
-                // random r and theta around centre
-                Agent {
-                    pos: Pos::new(
-                        random_range(self.win.left(), self.win.right()),
-                        random_range(self.win.bottom(), self.win.top()),
-                    ),
-                    step_size: 1f32,
-                }
-            })
+            .map(|_| Agent::new(self.win))
             .collect();
     }
 
@@ -98,15 +101,22 @@ fn model(app: &App) -> Model {
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    model
-        .agents
-        .iter_mut()
-        .for_each(|a| a.update(model.perlin, model.noise_scale, Pos::new(0.0, 0.0)));
+    model.agents.iter_mut().for_each(|a| {
+        a.update(
+            model.perlin,
+            model.noise_scale,
+            Pos::new(0.0, 0.0),
+            model.win,
+            app.duration,
+        )
+    });
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(BLACK);
+    draw.rect()
+        .wh(app.window_rect().wh())
+        .rgba(0.0, 0.0, 0.0, 0.02);
     // draw the newest agent set
     model.agents.iter().for_each(|agent| {
         draw.ellipse()
